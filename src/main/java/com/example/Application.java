d8b9e22f8c871b95b91bd83ddd3eadc225297bee
OHHYUN <omplcie@gmail.com>
2021-08-09 09:58:10 +0900

    add: redis

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
-@EnableRedisHttpSession
+//@EnableRedisHttpSession
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- src/main/java/com/example/web/RedisSampleController.java
+++ src/main/java/com/example/web/RedisSampleController.java
@@ -1,12 +1,21 @@
 package com.example.web;
 
+import com.example.config.auth.LoginUser;
+import com.example.config.auth.dto.SessionUser;
 import com.example.service.RedisSampleService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpSession;
+
 @RestController
+@RequiredArgsConstructor
 public class RedisSampleController {
+
+    private final HttpSession httpSession;
+
     @Autowired
     private RedisSampleService redisSampleService;
 
@@ -14,4 +23,15 @@ public class RedisSampleController {
     public void getRedisStringValue(String key) {
         redisSampleService.getRedisStringValue("banana");
     }
+
+    @GetMapping("/session")
+    public String getSession(){
+        System.out.println(httpSession.getId());
+        return httpSession.getId();
+    }
+    @GetMapping("/setsession")
+    public void setSession(){
+        httpSession.setAttribute("username", "testman");
+    }
+
 }
